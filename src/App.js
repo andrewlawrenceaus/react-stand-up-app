9b1234a7e27faa149cbbb88a051da001a3b03569
Andrew Lawrence <andrew.lawrence@resmed.com.au>
2023-08-07 12:38:22 +1000

    Fixed layout of run standups page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
-import { React, useState } from 'react';
-import ManageStandUp from './components/manage-stand-up/ManageStandUp';
-import RunStandUp from './components/run-stand-up/RunStandUp';
-import Header from './components/header/Header';
-
+import { React, useState } from "react";
+import ManageStandUp from "./components/manage-stand-up/ManageStandUp";
+import RunStandUp from "./components/run-stand-up/RunStandUp";
+import Header from "./components/header/Header";
+import { Container } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const App = () => {
+  const defaultStandUps = new Map();
+  defaultStandUps.set("Billabong", [
+    "Andrew L",
+    "Anthony",
+    "Evan",
+    "Jono",
+    "Kathleen",
+    "Kane",
+    "Sam",
+    "Nik",
+    "Tim",
+    "Nikunj",
+    "Andrew B",
+    "Tanvi",
+    "Grant",
+    "Sharon",
+    "David",
+  ]);
+  defaultStandUps.set("Soledad", ["Sharon", "Keerthi", "Chris"]);
 
-    const defaultStandUps = new Map();
-    defaultStandUps.set(
-        'Billabong', ['Andrew L', 'Anthony'
-        , 'Evan', 'Jono', 'Kathleen', 'Kane', 'Sam', 'Nik', 'Tim', 'Nikunj',
-        'Andrew B', 'Tanvi', 'Grant', 'Sharon', 'David']);
-    defaultStandUps.set(
-        'Soledad', ['Sharon', 'Keerthi', 'Chris']);
+  const [standUps, setStandUps] = useState(defaultStandUps);
+  const [activeStandUp, setActiveStandUp] = useState("Billabong");
+  const [activePage, setActivePage] = useState("Manage Stand-Ups");
 
-    const [standUps, setStandUps] = useState(defaultStandUps);
-    const [activeStandUp, setActiveStandUp] = useState('Billabong');
-    const [activePage, setActivePage] = useState('Manage Stand-Ups');
-
-    const pageButtonClickHandler = page => {
-        setActivePage(page);
-        console.log(page);
-    }
-    return (
-        <div>
-            <Header onPageButtonClick={pageButtonClickHandler}></Header>
-            {activePage === 'Manage Stand-Ups' ?
-                <ManageStandUp standUps={standUps}
-                    setStandUps={setStandUps}
-                    setActiveStandUp={setActiveStandUp}></ManageStandUp>
-                :
-                <RunStandUp standUp={standUps.get(activeStandUp)}
-                    activeStandUp={activeStandUp}></RunStandUp>
-            }
-        </div>
-    );
+  const pageButtonClickHandler = (page) => {
+    setActivePage(page);
+    console.log(page);
+  };
+  return (
+    <div>
+      <Header onPageButtonClick={pageButtonClickHandler}></Header>
+      <Container>
+        {activePage === "Manage Stand-Ups" ? (
+          <ManageStandUp
+            standUps={standUps}
+            setStandUps={setStandUps}
+            setActiveStandUp={setActiveStandUp}
+          ></ManageStandUp>
+        ) : (
+          <RunStandUp
+            standUp={standUps.get(activeStandUp)}
+            activeStandUp={activeStandUp}
+          ></RunStandUp>
+        )}
+      </Container>
+    </div>
+  );
 };
 
 export default App;
--- src/components/run-stand-up/RunStandUp.js
+++ src/components/run-stand-up/RunStandUp.js
@@ -1,12 +1,15 @@
-import { React, useState } from 'react';
-import Typography from '@mui/material/Typography';
-import AttendeeCard from './AttendeeCard';
-import StartStandUpCard from './StartStandUpCard';
-import StandUpCompleteCard from './StandUpCompleteCard';
+import { React, useState } from "react";
+import Typography from "@mui/material/Typography";
+import AttendeeCard from "./AttendeeCard";
+import StartStandUpCard from "./StartStandUpCard";
+import StandUpCompleteCard from "./StandUpCompleteCard";
+import { Grid } from "@mui/material";
 
 export default function RunStandUp(props) {
-  const [standUpAttendees, setStandUpAttendees] = useState(initialiseStandUpAttendees(props.standUp));
-  const [standUpStatus, setStandUpStatus] = useState('Ready');
+  const [standUpAttendees, setStandUpAttendees] = useState(
+    initialiseStandUpAttendees(props.standUp)
+  );
+  const [standUpStatus, setStandUpStatus] = useState("Ready");
 
   const passDuckHandler = () => {
     let updatedAttendees = standUpAttendees.slice();
@@ -15,56 +18,75 @@ export default function RunStandUp(props) {
       setStandUpAttendees(updatedAttendees);
     } else {
       setStandUpAttendees(initialiseStandUpAttendees(props.standUp));
-      setStandUpStatus('Complete')
+      setStandUpStatus("Complete");
     }
-  }
+  };
   const lateAttendeeHandler = () => {
     let updatedAttendees = standUpAttendees.slice();
     let lateAttendee = updatedAttendees.splice(0, 1);
     updatedAttendees.push(...lateAttendee);
     setStandUpAttendees(updatedAttendees);
-  }
+  };
 
   const startStandUpHandler = () => {
-    setStandUpStatus('In Progress');
-  }
+    setStandUpStatus("In Progress");
+  };
 
   const resetStandUpHandler = () => {
-    setStandUpStatus('Ready');
-  }
+    setStandUpStatus("Ready");
+  };
 
   return (
-    <>
-      <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
-        Run {props.activeStandUp} Stand Up
-      </Typography>
-      {(() => {
-        switch (standUpStatus) {
-          case 'Ready':
-            return <StartStandUpCard startStandUpHandler={startStandUpHandler}></StartStandUpCard>
-          case 'In Progress':
-            return <AttendeeCard attendee={standUpAttendees[0]}
-              passDuckHandler={passDuckHandler}
-              lateAttendeeHandler={lateAttendeeHandler}>
-            </AttendeeCard>
-          case 'Complete':
-            return <StandUpCompleteCard resetStandUpHandler={resetStandUpHandler}></StandUpCompleteCard>
-          default:
-            return null
-        }
-      }
-      )()}
-    </>
-  )
+    <Grid container>
+      <Grid item xs={12} display="flex" justifyContent="center" alignItems="center">
+        <Typography align="center" variant="h3" component="div" sx={{ flexGrow: 1 }}>
+          Run {props.activeStandUp} Stand Up
+        </Typography>
+      </Grid>
+    <Grid item xs={4} />
+      <Grid item xs={4}>
+        {(() => {
+          switch (standUpStatus) {
+            case "Ready":
+              return (
+                <StartStandUpCard
+                  startStandUpHandler={startStandUpHandler}
+                ></StartStandUpCard>
+              );
+            case "In Progress":
+              return (
+                <AttendeeCard
+                  attendee={standUpAttendees[0]}
+                  passDuckHandler={passDuckHandler}
+                  lateAttendeeHandler={lateAttendeeHandler}
+                ></AttendeeCard>
+              );
+            case "Complete":
+              return (
+                <StandUpCompleteCard
+                  resetStandUpHandler={resetStandUpHandler}
+                ></StandUpCompleteCard>
+              );
+            default:
+              return null;
+          }
+        })()}
+      </Grid>
+      <Grid item xs={4} />
+    </Grid>
+    
+  );
 }
 
 const initialiseStandUpAttendees = (attendees) => {
   let standUpSessionAttendees = [];
   let attendeesToAdd = [...attendees];
   while (attendeesToAdd.length > 0) {
-    let randomAttendee = attendeesToAdd
-      .splice(Math.floor(Math.random() * attendeesToAdd.length), 1)
+    let randomAttendee = attendeesToAdd.splice(
+      Math.floor(Math.random() * attendeesToAdd.length),
+      1
+    );
     standUpSessionAttendees.push(...randomAttendee);
   }
   return standUpSessionAttendees;
-}
+};
