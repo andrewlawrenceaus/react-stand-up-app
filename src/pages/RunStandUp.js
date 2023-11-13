9885a323fc5c694ece3827d4c5ab50b31d43a686
Andrew Lawrence <andrew.lawrence@resmed.com.au>
2023-11-13 16:08:55 +1100

    Removed unused imports, fixed formatting

--- src/pages/RunStandUp.js
+++ src/pages/RunStandUp.js
@@ -1,13 +1,15 @@
-import { useRouteLoaderData, useSearchParams } from 'react-router-dom';
+import { useRouteLoaderData } from 'react-router-dom';
 import RunStandUp from '../components/run-stand-up/RunStandUp';
 
 function RunStandUpPage() {
   const standUps = useRouteLoaderData('root');
   console.log(standUps);
-//   const activeStandUp = useSearchParams();
-const firstStandUp = standUps.entries().next().value;
+  //   const activeStandUp = useSearchParams();
+  const firstStandUp = standUps.entries().next().value;
 
-  return <RunStandUp activeStandUp={firstStandUp[0]} standUp={firstStandUp[1]}  />;
+  return (
+    <RunStandUp activeStandUp={firstStandUp[0]} standUp={firstStandUp[1]} />
+  );
 }
 
 export default RunStandUpPage;
